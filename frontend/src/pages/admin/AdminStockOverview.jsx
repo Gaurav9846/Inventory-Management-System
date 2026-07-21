6f6ae359749a28dc35962a632037f375a91c8bb9
Gaurav9846 <gurunggaurav1611@gmail.com>
2026-07-21 13:06:02 +0545

    import issue solved

--- frontend/src/pages/admin/AdminStockOverview.jsx
+++ frontend/src/pages/admin/AdminStockOverview.jsx
@@ -1,5 +1,5 @@
 // src/pages/admin/AdminStockOverview.jsx
-import ManagerStockOverview from "@/pages/Manager/ManagerStockOverview.jsx";
+import ManagerStockOverview from "@/pages/manager/ManagerStockOverview.jsx";
 
 export default function AdminStockOverview() {
   return <ManagerStockOverview />;
